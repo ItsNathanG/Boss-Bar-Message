efee68b7db65b740cae444dbd574f9e2afdb78d6
Nathan Glover <contact@itsnathang.me>
2018-02-16 14:08:23 -0500

    initial plugin structure

---
+++ src/main/java/me/itsnathang/bossbarmessage/BossBarMessage.java
@@ -0,0 +1,12 @@
+package me.itsnathang.bossbarmessage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BossBarMessage extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        this.getLogger().info("Hello world!");
+    }
+
+}
