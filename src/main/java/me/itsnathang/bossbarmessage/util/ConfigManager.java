2e78b8e80afb71263426872baa7d83a3494adf5f
Nathan Glover <contact@itsnathang.me>
2018-02-16 18:06:42 -0500

    Added configuration files

---
+++ src/main/java/me/itsnathang/bossbarmessage/util/ConfigManager.java
@@ -0,0 +1,45 @@
+package me.itsnathang.bossbarmessage.util;
+
+import me.itsnathang.bossbarmessage.BossBarMessage;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ConfigManager {
+    private static YamlConfiguration config, data;
+    private static File configFile, dataFile;
+
+    // Try to load configuration files.
+    public ConfigManager(BossBarMessage plugin) {
+        configFile = new File(plugin.getDataFolder() + File.separator + "config.yml");
+        dataFile = new File(plugin.getDataFolder() + File.separator + "data.yml");
+
+        if (!configFile.exists())
+            plugin.saveResource("config.yml", true);
+
+        if (!dataFile.exists())
+            plugin.saveResource("data.yml", true);
+
+        config = YamlConfiguration.loadConfiguration(configFile);
+        data = YamlConfiguration.loadConfiguration(dataFile);
+    }
+
+    public static boolean saveConfig() {
+        try {
+            config.save(configFile);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public static boolean saveData() {
+        try {
+            data.save(dataFile);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+}
