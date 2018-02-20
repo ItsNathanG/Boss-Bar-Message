b2fe1130881b0f67f404832dc561b33a227f1cd1
Nathan Glover <contact@itsnathang.me>
2018-02-19 23:00:35 -0500

    PlaceholderAPI Support

--- src/main/java/me/itsnathang/bossbarmessage/util/BossBarHandler.java
+++ src/main/java/me/itsnathang/bossbarmessage/util/BossBarHandler.java
@@ -1,5 +1,6 @@
 package me.itsnathang.bossbarmessage.util;
 
+import me.clip.placeholderapi.PlaceholderAPI;
 import me.itsnathang.bossbarmessage.BossBarMessage;
 import me.itsnathang.bossbarmessage.config.ConfigManager;
 import org.bukkit.Bukkit;
@@ -10,13 +11,20 @@ import org.bukkit.entity.Player;
 
 public class BossBarHandler {
     private static BossBarMessage plugin;
+    private static Boolean placeholder_api;
 
-    public BossBarHandler(BossBarMessage p) {
-        plugin = p;
+    public BossBarHandler(BossBarMessage plugin) {
+        BossBarHandler.plugin = plugin;
+
+        // PlaceholderAPI hook
+        placeholder_api = plugin.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI");
     }
 
-    // Send timed bar.
+    // Send timed bar
     public static void sendBar(Player player, BarColor color, BarStyle style, int time, String message) {
+        if (placeholder_api)
+            message = PlaceholderAPI.setPlaceholders(player, message);
+
         BossBar bar = Bukkit.createBossBar(message, color, style);
 
         bar.addPlayer(player);
@@ -30,7 +38,11 @@ public class BossBarHandler {
     public static void sendGlobal(BarColor color, BarStyle style, int time, String message) {
         BossBar bar = Bukkit.createBossBar(message, color, style);
 
-        Bukkit.getOnlinePlayers().forEach(bar::addPlayer);
+        Bukkit.getOnlinePlayers().forEach((player) -> {
+            bar.setTitle(PlaceholderAPI.setPlaceholders(player, message));
+
+            bar.addPlayer(player);
+        });
 
         new BarCountdown(bar, time).runTaskTimer(plugin, 0L, 20L);
 
