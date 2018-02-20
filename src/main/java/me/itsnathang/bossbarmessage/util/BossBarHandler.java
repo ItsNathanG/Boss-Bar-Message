b47e4f450a9cf49228cafd94ed0f99602844ac42
Nathan Glover <contact@itsnathang.me>
2018-02-19 22:10:38 -0500

    Boss Bar Creation

--- src/main/java/me/itsnathang/bossbarmessage/util/BossBarHandler.java
+++ src/main/java/me/itsnathang/bossbarmessage/util/BossBarHandler.java
@@ -1,35 +1,51 @@
 package me.itsnathang.bossbarmessage.util;
 
+import me.itsnathang.bossbarmessage.BossBarMessage;
+import me.itsnathang.bossbarmessage.config.ConfigManager;
 import org.bukkit.Bukkit;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitTask;
 
 public class BossBarHandler {
+    private static BossBarMessage plugin;
+
+    public BossBarHandler(BossBarMessage p) {
+        plugin = p;
+    }
 
     // Send boss bar to an individual.
-    public static void sendBar(String name, Player player) {
-        BossBar bar = Bukkit.createBossBar("", BarColor.BLUE, BarStyle.SOLID);
+    public static BossBar sendBar(String message, Player player, BarColor color) {
+        BossBar bar = Bukkit.createBossBar(message, color, BarStyle.SOLID);
 
-        bar.setTitle(ConfigManager.getMessage(name));
-        bar.setColor(ConfigManager.getColor(name));
+        //bar.setTitle(ConfigManager.getMessage(name));
+        //bar.setColor(ConfigManager.getColor(name));
 
         bar.addPlayer(player);
+
+        return bar;
     }
 
     // Send timed bar.
-    public static void sendBar(String name, Player player, int time) {
-        Runnable boss_bar = () -> sendBar(name, player);
+    public static BossBar sendBar(Player player, BarColor color, BarStyle style, int time, String message) {
+        BossBar bar = Bukkit.createBossBar(message, color, style);
 
-        BukkitTask.runTask(getPlugin(), boss_bar);
+        bar.addPlayer(player);
+
+        Runnable remove_bar = bar::removeAll;
+
+        new BarCountdown(bar, time).runTaskTimer(plugin, 0L, 20L);
+
+        plugin.getServer().getScheduler().runTaskLater(plugin, remove_bar, time * 20);
+
+        return bar;
     }
 
     // Send boss bar to everyone one server.
     public static void sendGlobal(String name) {
         for (Player player : Bukkit.getOnlinePlayers()) {
-            sendBar(name, player);
+            //sendBar(name, player);
         }
     }
 
