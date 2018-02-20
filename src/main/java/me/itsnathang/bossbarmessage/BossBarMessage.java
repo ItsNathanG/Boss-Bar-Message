d8f1557a95a2a796bb42f1e1b17e35f0d013b6dd
Nathan Glover <contact@itsnathang.me>
2018-02-19 23:01:03 -0500

    Removed "Hello world!"

--- src/main/java/me/itsnathang/bossbarmessage/BossBarMessage.java
+++ src/main/java/me/itsnathang/bossbarmessage/BossBarMessage.java
@@ -6,18 +6,14 @@ import me.itsnathang.bossbarmessage.config.ConfigManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BossBarMessage extends JavaPlugin {
-    public static BossBarMessage plugin;
 
     @Override
     public void onEnable() {
-        this.getLogger().info("Hello world!");
-
-        plugin = this;
-
         getCommand("bossbarmessage").setExecutor(new CommandHandler(this));
 
         // Initialize Configuration Manager
         new ConfigManager(this);
+
         new BossBarHandler(this);
     }
 
