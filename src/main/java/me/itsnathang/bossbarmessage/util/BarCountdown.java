ce33fbf6463be89b5d21f7589b7ca707772204e3
Nathan Glover <contact@itsnathang.me>
2018-02-19 22:10:09 -0500

    Cool bar countdown!

--- src/main/java/me/itsnathang/bossbarmessage/util/BarCountdown.java
+++ src/main/java/me/itsnathang/bossbarmessage/util/BarCountdown.java
@@ -1,4 +1,27 @@
 package me.itsnathang.bossbarmessage.util;
 
-public class BarCountdown {
+import org.bukkit.boss.BossBar;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class BarCountdown extends BukkitRunnable {
+    private final BossBar bar;
+    private Integer time_left;
+    private Integer total_time;
+
+    public BarCountdown(BossBar b, int times) {
+        this.bar = b;
+        this.total_time = times;
+        this.time_left = times;
+    }
+
+    @Override
+    public void run() {
+        if (time_left > 0) {
+            bar.setProgress(time_left.doubleValue() / total_time.doubleValue());
+
+            time_left--;
+        } else {
+            this.cancel();
+        }
+    }
 }
